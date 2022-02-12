a5d0d1516be9feea84fd0b9b05e60c18d6869821
Slavil <89082731+Slavil@users.noreply.github.com>
2022-02-12 09:08:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel__button--right')
+const previousButton = document.querySelector('.carousel__button--left')
+const dotsNavs = document.querySelector('.carousel__nav');
+const dots = Array.from(dotsNavs.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+const setSlidePosition = (slide, index)=>{
+    slide.style.left = slideWidth * index + 'px'
+};
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide)=>{
+    track.style.transform = 'translateX(-' + targetSlide.style.left +")";
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+dotsNavs.addEventListener('click', e=>{
+    const targetDot = e.target.closest('button');
+    if(!targetDot) return;
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNavs.querySelector('.current-slide')
+    const targetIndex = dots.findIndex(dot=>dot === targetDot);
+    const targetSlide = slides[targetIndex]
+    moveToSlide(track, currentSlide, targetSlide)
+    currentDot.classList.remove('current-slide')
+    targetDot.classList.add('current-slide')
+})
+
